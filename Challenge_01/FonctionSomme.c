2060958c8f573ca1b49f07f9881119e66c17f907
Karim Benkhira <karimbenkhira.contact@gmail.com>
2024-09-13 16:17:48 +0200

    Fonction de Somme

---
+++ Challenge_01/FonctionSomme.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int  FonctiondeSomme(int Number1,int Number2)
+{
+    return Number1 + Number2;
+}
+/*
+int main()
+{
+    int Number1,Number2;
+    printf("Enter Number 1 : ");
+    scanf("%d",&Number1);
+    printf("Enter Number 2 : ");
+    scanf("%d",&Number2);
+
+    int sum = FonctiondeSomme(Number1,Number2);
+
+    printf("Sum = %d\n",sum);
+
+    return 0;
+}
+*/
